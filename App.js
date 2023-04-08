2d04382456539bc341e9f08f5f4fa6fa08f8c03d
jpalencia77 <jpalencia@ucol.mx>
2023-04-08 17:58:28 -0500

    FocusTime finished - Expo

--- App.js
+++ App.js
@@ -13,19 +13,16 @@ import { Focus } from './src/features/Focus';
 import { Timer } from './src/features/Timer';
 import { FocusHistory } from './src/features/FocusHistory';
 
-export default function App() {
-  const [currentSubject, setCurrentSubject] = useState('');
-  const [history, setHistory] = useState([''])
 
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState();
+  const [history, setHistory] = useState([]);
   return (
     <SafeAreaView style={styles.container}>
       {!currentSubject ? (
-
         <>
-        
-        <Focus addSubject={setCurrentSubject} />
-        <FocusHistory history={history} />
-
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
         </>
       ) : (
         <Timer 
